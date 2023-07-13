47583b34761a12fda0a8a6d76cbdbf84f1da3958
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-07-13 14:39:42 +0100

    Write last C03 exercises, create C04 file

--- TPIL4/Chapter03.lean
+++ TPIL4/Chapter03.lean
@@ -141,3 +141,20 @@ intros hpq hnq hp
 have hq : q := hpq hp
 contradiction
 
+/-
+open Classical
+
+variable (p q r : Prop)
+
+example : (p → q ∨ r) → ((p → q) ∨ (p → r)) := sorry
+example : ¬(p ∧ q) → ¬p ∨ ¬q := sorry
+example : ¬(p → q) → p ∧ ¬q := sorry
+example : (p → q) → (¬p ∨ q) := sorry
+example : (¬q → ¬p) → (p → q) := sorry
+example : p ∨ ¬p := sorry
+example : (((p → q) → p) → p) := sorry
+-/
+
+/-
+Prove ¬(p ↔ ¬p) without using classical reasoning.
+-/
