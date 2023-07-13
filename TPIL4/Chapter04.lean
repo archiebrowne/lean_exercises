cb8fc7260e5d08b2251341df547fa7ff5553c8ab
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-07-13 16:23:52 +0100

    Exercise02

--- TPIL4/Chapter04.lean
+++ TPIL4/Chapter04.lean
@@ -4,7 +4,6 @@ variable (α : Type) (p q : α → Prop)
 
 -- Exercise 01
 
-
 example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
 apply Iff.intro
 ·intro h
@@ -26,22 +25,45 @@ cases h with
 |inl hp => apply Or.inl; exact hp x
 |inr hq => apply Or.inr; exact hq x
 
-
 -- Exercise 02
 
 variable (r : Prop)
 
-/-
-example : α → ((∀ x : α, r) ↔ r) := sorry
-example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
-example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
--/
+example : α → ((∀ x : α, r) ↔ r) := by
+intro hα
+apply Iff.intro
+·intro h; exact h hα
+·intro h; intro; exact h
 
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := by
+apply Iff.intro
+·intro h
+ apply Or.elim (Classical.em r)
+ ·intro hr; apply Or.inr; exact hr
+ ·intro hnr 
+  apply Or.inl
+  intro x
+  specialize h x
+  cases h with
+  |inl hp => exact hp
+  |inr hr => exfalso; contradiction
+·intro h
+ cases h with
+ |inl hp => intro x; apply Or.inl; exact hp x
+ |inr hr => intro x; apply Or.inr; exact hr
 
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := by
+apply Iff.intro
+·intros hrp hr x
+ specialize hrp x
+ exact hrp hr
+·intros h x hr
+ exact h hr x
+ 
 -- Exercise 03
 
-variable (men : Type) (barber : men)
-variable (shaves : men → men → Prop)
+-- variable (men : Type) (barber : men)
+-- variable (shaves : men → men → Prop)
 
 /-
 example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := sorry
