875503377ba632d957a04a1fe81752eba6e173de
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-07-13 15:29:50 +0100

    fa x (p x and q x) iff (fa x p x) and (fa x q x)

--- TPIL4/Chapter04.lean
+++ TPIL4/Chapter04.lean
@@ -4,8 +4,18 @@ variable (α : Type) (p q : α → Prop)
 
 -- Exercise 01
 
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
+apply Iff.intro
+·intro h
+ constructor <;> intro x; exact (h x).1; exact (h x).2
+·intro h
+ intro x; constructor; exact h.1 x; exact h.2 x
+ 
+
+
+
 /-
-example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
 example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
 example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
 -/
