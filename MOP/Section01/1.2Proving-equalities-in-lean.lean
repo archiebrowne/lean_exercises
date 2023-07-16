a3788e77ef675fe6c9701a70415d35f6a0d55561
Archie Browne <archibald.browne21@imperial.ac.uk>
2023-07-16 16:22:56 +0100

    MOP section 1.2

---
+++ MOP/Section01/1.2Proving-equalities-in-lean.lean
@@ -0,0 +1,27 @@
+import Mathlib.Tactic
+
+example {r s : ℝ} (h1 : s = 3) (h2 : r + 2 * s = -1) : r = -7 :=
+  calc
+    r = r + 2 * s - 2 * s := by ring
+    _ = -1 - 2 * s := by rw [h2]
+    _ = -1 - 2 * 3 := by rw [h1]
+    _ = -7 := by ring
+
+example {a b m n : ℤ} (h1 : a * m + b * n = 1) (h2 : b ^ 2 = 2 * a ^ 2) :
+    (2 * a * n + b * m) ^ 2 = 2 :=
+  calc
+    (2 * a * n + b * m) ^ 2
+      = 2 * (a * m + b * n) ^ 2 + (m ^ 2 - 2 * n ^ 2) * (b ^ 2 - 2 * a ^ 2) := by ring
+    _ = 2 * 1 ^ 2 + (m ^ 2 - 2 * n ^ 2) * (2 * a ^ 2 - 2 * a ^ 2) := by rw [h1, h2]
+    _ = 2 := by ring
+  
+example {a b c d e f : ℤ} (h1 : a * d = b * c) (h2 : c * f = d * e) :
+    d * (a * f - b * e) = 0 := by
+  calc
+    d * (a * f - b * e) = d * a * f - d * b * e := by ring
+    _ = a * d * f - d * e * b := by ring
+    _ = b * c * f - c * f * b := by rw [h1, h2]
+    _ = 0 := by ring
+  
+
+  
